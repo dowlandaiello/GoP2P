71b93ced9319c09635ccb439216c131e733db039
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-07-30 21:25:22 -0400

    Added node initialization unit testing.

--- types/node/node_test.go
+++ types/node/node_test.go
@@ -1,1 +1,30 @@
 package node
+
+import (
+	"testing"
+
+	"github.com/mitsukomegumi/GoP2P/common"
+)
+
+// TestNewNode - test functionality of node initialization method
+func TestNewNode(t *testing.T) {
+	address, err := common.GetExtIPAddrWithUpNP() // Attempt to fetch current external IP address
+
+	if err != nil { // Check for errors
+		err = nil // Reset error
+
+		address, err = common.GetExtIPAddrWithoutUpNP() // Attempt to fetch address without UpNP
+
+		if err != nil { // Check second try for errors
+			t.Errorf(err.Error()) // Return found error
+			t.FailNow()
+		}
+	}
+
+	_, err = NewNode(address, true) // Attempt to create new node
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Return found error
+		t.FailNow()
+	}
+}
