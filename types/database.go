a8d2611e43d26f54a6c89f8cf0d23424c5fe4ee6
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-07-30 20:22:40 -0400

    Added database types, methods.

---
+++ types/database.go
@@ -0,0 +1,55 @@
+package types
+
+import (
+	"errors"
+
+	"github.com/mitsukomegumi/GoP2P/common"
+)
+
+// NodeDatabase - database containing list of node addresses, as well as bootstrap addresses
+type NodeDatabase struct {
+	Nodes *[]Node // Nodes - primary list of nodes
+
+	AcceptableTimeout uint // AcceptableTimeout - database-wide definition for operation timeout
+}
+
+// AddNode - adds node to specified nodedatabase, after checking address of node
+func (db *NodeDatabase) AddNode(node *Node) error {
+	if !common.CheckAddress(node.Address) {
+		return errors.New("invalid address")
+	}
+
+	*db.Nodes = append(*db.Nodes, *node)
+
+	return nil
+}
+
+// RemoveNode - removes node with specified address from database
+func (db *NodeDatabase) RemoveNode(address string) error {
+	nodeIndex, err := db.QueryForAddress(address) // Finds index of node with address
+
+	if err != nil { // Checks for error
+		return err // Returns error
+	}
+
+	db.remove(int(nodeIndex)) // Removes value at index
+	return nil                // Returns nil, no error
+}
+
+func (db *NodeDatabase) remove(s int) { // Removes address at index
+	*db.Nodes = append((*db.Nodes)[:s], (*db.Nodes)[s+1:]...) // Remove index
+}
+
+// QueryForAddress - attempts to search specified node database for specified address, returns index of node
+func (db *NodeDatabase) QueryForAddress(address string) (uint, error) {
+	x := 0
+
+	for x != len(*db.Nodes) { // Wait until entire db has been queried
+		if address == (*db.Nodes)[x].Address { // Check for match
+			return uint(x), nil // If provided value matches value of node in list, return index
+		}
+		x++ // Increment index
+	}
+
+	return 0, errors.New("no value found")
+}
