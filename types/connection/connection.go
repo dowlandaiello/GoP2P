b3ba07f55a36c0145f7b7d15166afe8d48caaceb
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-07-31 13:07:31 -0400

    Added initialization methods for connection types.

--- types/connection/connection.go
+++ types/connection/connection.go
@@ -1,6 +1,12 @@
 package connection
 
-import "github.com/mitsukomegumi/GoP2P/types/node"
+import (
+	"errors"
+	"reflect"
+	"strings"
+
+	"github.com/mitsukomegumi/GoP2P/types/node"
+)
 
 var (
 	// AvailableConnectionTypes - global preset list of types connections that can be made
@@ -23,11 +29,10 @@ var (
 
 // Connection - abstract container for Golang connection type, contains metadata, routing parameteres
 type Connection struct {
-	DestinationNode    string     `json:"destination node"`  // Node to contact
+	DestinationNode    *node.Node `json:"destination node"`  // Node to contact
 	InitializationNode *node.Node `json:"initializing node"` // Node initializing connection
 
-	Metadata Metadata `json:"meta"` // Connection metadata
-	Data     []byte   `json:"data"` // Actual data being transmitted
+	Data []byte `json:"data"` // Actual data being transmitted
 
 	ConnectionType  string  `json:"type"` // Type of connection
 	ConnectionStack []Event `json:"stack"`
@@ -36,8 +41,33 @@ type Connection struct {
 // Event - container holding metadata concerning a direction given from a peer to another
 type Event struct {
 	EventType string `json:"type"`
+
+	Data []byte `json:"data"` // Data being transmitted
+
+	SourceNode      *node.Node `json:"source"`      // Initialization node
+	DestinationNode *node.Node `json:"destination"` // Node to contact
 }
 
-// Metadata - container for connection metadata
-type Metadata struct {
+// NewConnection - creates new Connection{} instance with specified data, peers
+func NewConnection(destinationNode *node.Node, sourceNode *node.Node, data []byte, connectionType string, connectionStack []Event) (*Connection, error) {
+	if strings.ToLower(connectionType) != "relay" && strings.ToLower(connectionType) != "pointer" { // Check connection type is valid
+		return &Connection{}, errors.New("invalid connection type") // Error occurred, return nil
+	} else if reflect.ValueOf(destinationNode).IsNil() || reflect.ValueOf(sourceNode).IsNil() { // Check that peer values aren't nil
+		return &Connection{}, errors.New("invalid peer value") // Peer values nil, return nil constructor
+	} else if len(data) == 0 { // Check that data is being passed trough
+		return &Connection{}, errors.New("invalid data") // Return error
+	}
+
+	return &Connection{DestinationNode: destinationNode, InitializationNode: sourceNode, Data: data, ConnectionType: connectionType, ConnectionStack: connectionStack}, nil // No error occurred, return correctly initialized Connection
+}
+
+// NewEvent - creates new Event{} instance with specified data, peers
+func NewEvent(eventType string, data []byte, sourceNode *node.Node, destinationNode *node.Node) (*Event, error) {
+	if strings.ToLower(eventType) != "push" && strings.ToLower(eventType) != "fetch" { // Check for invalid types
+		return &Event{}, errors.New("invalid event") // Error occurred, return nil, error
+	} else if reflect.ValueOf(sourceNode).IsNil() || reflect.ValueOf(destinationNode).IsNil() { // Check for invalid peer values
+		return &Event{}, errors.New("invalid peer value") // Error occurred, return nil, error
+	}
+
+	return &Event{EventType: eventType, Data: data, SourceNode: sourceNode, DestinationNode: destinationNode}, nil // Return initialized event
 }
