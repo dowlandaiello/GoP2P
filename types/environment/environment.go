8d5d1d440c00e8de1e5ebde28a1a31ced46999a2
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-01 17:41:41 -0400

    Added environment initialization function.

--- types/environment/environment.go
+++ types/environment/environment.go
@@ -1,8 +1,16 @@
 package environment
 
+import (
+	"errors"
+	"reflect"
+
+	"github.com/mitsukomegumi/GoP2P/types/node"
+)
+
 // Environment - abstract container holding variables, configurations of a certain node
 type Environment struct {
-	EnvironmentVariables []*Variable
+	EnvironmentVariables []*Variable `json:"variables"`
+	EnvironmentNode      *node.Node  `json:"node"`
 }
 
 // Variable - container holding a variable's data (pointer), and identification properties (id, type)
@@ -10,4 +18,13 @@ type Variable struct {
 	VariableType       string       `json:"type"`       // VariableType - type of variable (e.g. string, block, etc...)
 	VariableIdentifier string       `json:"identifier"` // VariableIdentifier - id of variable (used for querying)
 	VariableData       *interface{} `json:"data"`       // VariableData - pretty self-explanatory (usually a pointer to a struct)
+}
+
+// NewEnvironment - creates new instance of environment struct with specified node value
+func NewEnvironment(node *node.Node) error {
+	if reflect.ValueOf(node).IsNil() { // Check that node is not nil
+		return errors.New("invalid node") // Return error if true
+	}
+
+	return nil // No error occurred, return nil
 }
