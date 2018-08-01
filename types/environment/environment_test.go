cf439153613427bf5f2747fe5848547080d9d0c3
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-01 18:01:41 -0400

    Finished adding environment unit testing. Working on testing for vars.

--- types/environment/environment_test.go
+++ types/environment/environment_test.go
@@ -1,1 +1,51 @@
 package environment
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/mitsukomegumi/GoP2P/common"
+	"github.com/mitsukomegumi/GoP2P/types/node"
+)
+
+func TestNewEnvironment(t *testing.T) {
+	address, err := common.GetExtIPAddrWithUpNP() // Attempt to fetch current external IP address
+
+	if err != nil { // Check for errors
+		err = nil // Reset error
+
+		address, err = common.GetExtIPAddrWithoutUpNP() // Attempt to fetch address without UpNP
+
+		if err != nil { // Check second try for errors
+			t.Errorf(err.Error()) // Return found error
+			t.FailNow()
+		}
+	}
+
+	node, err := node.NewNode(address, true) // Attempt to create new node
+
+	if err != nil && !strings.Contains(err.Error(), "root") { // Check for errors
+		t.Errorf(err.Error()) // Return found error
+		t.FailNow()
+	} else if err != nil { // Account for special case
+		t.Logf(err.Error())
+	}
+
+	env, err := NewEnvironment(&node) // Initialize new environment
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	serializedEnv, err := common.SerializeToBytes(env) // Serialize environment
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	envID := common.SHA256(serializedEnv) // Get hash value of environment
+
+	t.Logf("create environment with ID %s", envID) // Log success
+}
