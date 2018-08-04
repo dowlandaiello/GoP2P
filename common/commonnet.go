2fabe760d6e775954551ad1bcb8ee37e094517ef
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-04 17:57:25 -0400

    Added connection closing, referenced connection sending.

--- common/commonnet.go
+++ common/commonnet.go
@@ -18,5 +18,22 @@ func SendBytes(b []byte, address string) error {
 		return err // Return found errors
 	}
 
+	err = connection.Close() // Close connection
+
+	if err != nil { // Check for errors
+		return err // Return found error
+	}
+
+	return nil // No error occurred, return nil
+}
+
+// SendBytesWithConnection - attempt to send specified bytes to given address via given connection
+func SendBytesWithConnection(connection *net.Conn, b []byte) error {
+	_, err := (*connection).Write(b) // Write to connection
+
+	if err != nil { // Check for errors
+		return err // Return found error
+	}
+
 	return nil // No error occurred, return nil
 }
