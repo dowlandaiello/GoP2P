846f8394917b24bcd3df390353a7dee2d98651b3
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-04 17:55:24 -0400

    Added SendBytes() unit testing.

--- common/commonnet_test.go
+++ common/commonnet_test.go
@@ -1,1 +1,15 @@
 package common
+
+import "testing"
+
+// TestSendBytes - test functionality of SendBytes() method
+func TestSendBytes(t *testing.T) {
+	err := SendBytes([]byte("test"), "1.1.1.1:80") // Write to address
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	t.Logf("wrote to address 1.1.1.1") // Log success
+}
