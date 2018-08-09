b21f95b6801350b64520a0e67c8ecc5ce69af601
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-09 17:56:14 -0400

    Added command unit testing.

--- types/command/command_test.go
+++ types/command/command_test.go
@@ -1,1 +1,14 @@
 package command
+
+import "testing"
+
+// TestNewCommand - test functionality of NewCommand() initializer
+func TestNewCommand(t *testing.T) {
+	command, err := NewCommand("test", &Modifiers{}) // Attempt to initialize command
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+	}
+
+	t.Logf("found command %s", command) // Log success
+}
