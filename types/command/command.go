8636ec6d8f65596c73b35d0b432f120e06a9230c
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-09 14:11:22 -0400

    Added command init method.

--- types/command/command.go
+++ types/command/command.go
@@ -4,11 +4,20 @@ package command
 type Command struct {
 	Command string `json:"command"`
 
-	Modifiers string `json:"modifiers"`
+	Modifiers *Modifiers `json:"modifiers"`
 }
 
 // Modifiers - abstract containers holding specific parameters for a command
 type Modifiers struct {
 	Type  string      `json:"type"`
 	Value interface{} `json:"value"`
+}
+
+/*
+	BEGIN EXPORTED METHODS:
+*/
+
+// NewCommand - attempt to initialize new instance of command struct with specified command, modifiers.
+func NewCommand(command string, modifiers *Modifiers) (*Command, error) {
+	return &Command{}, nil
 }
