bf4ed3e768455fbe45f6ef3e6ea011ddb557a6ce
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-09 13:52:10 -0400

    Backup.

--- types/command/command.go
+++ types/command/command.go
@@ -3,5 +3,12 @@ package command
 // Command - absctract container holding command values
 type Command struct {
 	Command string `json:"command"`
-	
+
+	Modifiers string `json:"modifiers"`
+}
+
+// Modifiers - abstract containers holding specific parameters for a command
+type Modifiers struct {
+	Type  string      `json:"type"`
+	Value interface{} `json:"value"`
 }
