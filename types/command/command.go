690e58e77b1a1293287a777bc393e059bb0f0183
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-09 14:23:10 -0400

    Added a few comments, error checking.

--- types/command/command.go
+++ types/command/command.go
@@ -1,5 +1,10 @@
 package command
 
+import (
+	"errors"
+	"reflect"
+)
+
 // Command - absctract container holding command values
 type Command struct {
 	Command string `json:"command"`
@@ -19,5 +24,11 @@ type Modifiers struct {
 
 // NewCommand - attempt to initialize new instance of command struct with specified command, modifiers.
 func NewCommand(command string, modifiers *Modifiers) (*Command, error) {
-	return &Command{}, nil
+	if command == "" { // Check for nil command
+		return &Command{}, errors.New("invalid command") // Return found error
+	} else if reflect.ValueOf(modifiers).IsNil() { // Check for nil modifier
+		return &Command{}, errors.New("invalid modifier") // Return found error
+	}
+
+	return &Command{Command: command, Modifiers: modifiers}, nil // Return instance
 }
