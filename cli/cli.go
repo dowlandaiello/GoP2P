8546b65274558389b1a2436f80c1211170bd2ec4
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-11 14:07:31 -0400

    CLI node wrapper.

--- cli/cli.go
+++ cli/cli.go
@@ -1,1 +1,41 @@
 package cli
+
+import (
+	"github.com/mitsukomegumi/GoP2P/common"
+	"github.com/mitsukomegumi/GoP2P/types/node"
+)
+
+// NewNode - simple wrapper for initializing a new node
+func NewNode() (*node.Node, error) {
+	address, err := common.GetExtIPAddrWithUpNP() // Attempt to fetch current external IP address
+
+	if err != nil { // Check for errors
+		err = nil // Reset error
+
+		address, err = common.GetExtIPAddrWithoutUpNP() // Attempt to fetch address without UpNP
+
+		if err != nil { // Check second try for errors
+			return nil, err
+		}
+	}
+
+	node, err := node.NewNode(address, true) // Attempt to create new node
+
+	if err != nil { // Check for errors
+		return nil, err
+	}
+
+	currentDir, err := common.GetCurrentDir() // Fetch current directory
+
+	if err != nil { // Check for errors
+		return nil, err // Return found error
+	}
+
+	err = node.WriteToMemory(currentDir) // Attempt to write node to memory
+
+	if err != nil { // Check for errors
+		return nil, err // Return found error
+	}
+
+	return &node, nil // No error occurred, return node
+}
--- cli/cli_test.go
+++ cli/cli_test.go
@@ -1,1 +1,15 @@
 package cli
+
+import "testing"
+
+// TestNewNode - test functionality of newnode wrapper method
+func TestNewNode(t *testing.T) {
+	node, err := NewNode() // Attempt to create new node
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	t.Logf("found node with address %s", node.Address) // Log success
+}
