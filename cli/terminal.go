445130aff62bc4aaec5caf83b9f95becd52de5b8
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-08-11 21:48:39 -0400

    Added variable types.

--- cli/terminal.go
+++ cli/terminal.go
@@ -8,7 +8,8 @@ import (
 
 // Terminal - absctract container holding set of variable with values (runtime only)
 type Terminal struct {
-	Variables []interface{}
+	Variables     []interface{}
+	VariableTypes []string
 }
 
 // NewTerminal - attempts to start io handler for term commands
@@ -30,18 +31,20 @@ func NewTerminal() error {
 }
 
 // AddVariable - attempt to append specified variable to terminal variable list
-func (term *Terminal) AddVariable(variable interface{}) error {
+func (term *Terminal) AddVariable(variable interface{}, variableType string) error {
 	if reflect.ValueOf(term).IsNil() { // Check for nil variable
 		return errors.New("nil terminal found") // Return error
 	}
 
 	if len(term.Variables) == 0 { // Check for uninitialized variable array
-		term.Variables = []interface{}{variable} // Initialize with variable
+		term.Variables = []interface{}{variable}    // Initialize with variable
+		term.VariableTypes = []string{variableType} // Initialize with type
 
 		return nil // No error occurred, return nil
 	}
 
-	term.Variables = append(term.Variables, variable) // Append to array
+	term.Variables = append(term.Variables, variable)             // Append to array
+	term.VariableTypes = append(term.VariableTypes, variableType) // Append to array
 
 	return nil // No error occurred, return nil
 }
