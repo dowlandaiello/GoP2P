29278d562ecd1b7af63feea31775779722f69fdd
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-20 20:50:52 -0400

    Dep ensured.

--- rpc/server.go
+++ rpc/server.go
@@ -1,4 +1,15 @@
 package rpc
 
+import (
+	"context"
+
+	proto "github.com/mitsukomegumi/gop2p/rpc/proto"
+)
+
 // Server - gop2p RPC server
 type Server struct{}
+
+// NewNode - node.NewNode RPC handler
+func (server *Server) NewNode(ctx context.Context, req *proto.NewNodeRequest) {
+
+}
