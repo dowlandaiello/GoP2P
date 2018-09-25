020164ceb15b4a855561be33ec0fa3dafee14314
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-25 13:01:36 -0400

    Init environment.NewEnvironment handler.

--- rpc/environment/server.go
+++ rpc/environment/server.go
@@ -2,8 +2,11 @@ package environment
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/mitsukomegumi/GoP2P/common"
 	environmentProto "github.com/mitsukomegumi/GoP2P/rpc/proto/environment"
+	"github.com/mitsukomegumi/GoP2P/types/environment"
 )
 
 // Server - GoP2P RPC server
@@ -11,5 +14,21 @@ type Server struct{}
 
 // NewEnvironment - environment.NewEnvironment RPC handler
 func (server *Server) NewEnvironment(ctx context.Context, req *environmentProto.GeneralRequest) (*environmentProto.GeneralResponse, error) {
+	currentDir, err := common.GetCurrentDir() // Fetch current directory
 
+	if err != nil { // Check for errors
+		return &environmentProto.GeneralResponse{}, err // Return found error
+	}
+
+	env, err := environment.ReadEnvironmentFromMemory(currentDir) // Attempt to read environment from memory
+
+	if err != nil { // Check for errors
+		env, err = environment.NewEnvironment() // Init environment
+
+		if err != nil { // Check for errors
+			return &environmentProto.GeneralResponse{}, err // Return found error
+		}
+	}
+
+	return &environmentProto.GeneralResponse{Message: fmt.Sprintf("\nInitialized node %v", env)}, nil // Return response
 }
