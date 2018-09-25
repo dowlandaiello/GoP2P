0af874bcf658004994aa31f07511c4d2eead796c
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-25 18:51:54 -0400

    Added querytype server handler.

--- rpc/environment/server.go
+++ rpc/environment/server.go
@@ -35,6 +35,31 @@ func (server *Server) NewEnvironment(ctx context.Context, req *environmentProto.
 	return &environmentProto.GeneralResponse{Message: fmt.Sprintf("\nInitialized environment %v", env)}, nil // Return response
 }
 
+// QueryType - environment.QueryType RPC handler
 func (server *Server) QueryType(ctx context.Context, req *environmentProto.GeneralRequest) (*environmentProto.GeneralResponse, error) {
+	currentDir, err := common.GetCurrentDir() // Fetch current directory
 
+	if err != nil { // Check for errors
+		return &environmentProto.GeneralResponse{}, err // Return found error
+	}
+
+	env, err := environment.ReadEnvironmentFromMemory(currentDir) // Attempt to read environment from memory
+
+	if err != nil { // Check for errors
+		env, err = environment.NewEnvironment() // Init environment
+
+		if err != nil { // Check for errors
+			return &environmentProto.GeneralResponse{}, err // Return found error
+		}
+
+		env.WriteToMemory(currentDir) // Save for persistency
+	}
+
+	foundVariable, err := env.QueryType(req.VariableType) // Query type
+
+	if err != nil { // Check for errors
+		return &environmentProto.GeneralResponse{}, err // Return found error
+	}
+
+	return &environmentProto.GeneralResponse{Message: fmt.Sprintf("\nFound variable %v", foundVariable)}, nil // No error occurred, return output
 }
