5d653290855c48561b6c461a1684a068a1a17640
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-25 20:06:14 -0400

    Added queryvalue handler.

--- rpc/environment/server.go
+++ rpc/environment/server.go
@@ -63,3 +63,32 @@ func (server *Server) QueryType(ctx context.Context, req *environmentProto.Gener
 
 	return &environmentProto.GeneralResponse{Message: fmt.Sprintf("\nFound variable %v", foundVariable)}, nil // No error occurred, return output
 }
+
+// QueryValue - environment.QueryValue RPC handler
+func (server *Server) QueryValue(ctx context.Context, req *environmentProto.GeneralRequest) (*environmentProto.GeneralResponse, error) {
+	currentDir, err := common.GetCurrentDir() // Fetch current directory
+
+	if err != nil { // Check for errors
+		return &environmentProto.GeneralResponse{}, err // Return found error
+	}
+
+	env, err := environment.ReadEnvironmentFromMemory(currentDir) // Attempt to read environment from memory
+
+	if err != nil { // Check for errors
+		env, err = environment.NewEnvironment() // Init environment
+
+		if err != nil { // Check for errors
+			return &environmentProto.GeneralResponse{}, err // Return found error
+		}
+
+		env.WriteToMemory(currentDir) // Save for persistency
+	}
+
+	foundVariable, err := env.QueryValue(req.Value) // Query type
+
+	if err != nil { // Check for errors
+		return &environmentProto.GeneralResponse{}, err // Return found error
+	}
+
+	return &environmentProto.GeneralResponse{Message: fmt.Sprintf("\nFound variable %v", foundVariable)}, nil // No error occurred, return output
+}
