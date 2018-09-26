65f2976ee10d96fffd3d828ca83d14dda2d0b8a8
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-26 07:59:29 -0400

    Added IO proto support.

--- rpc/proto/environment/environment.twirp.go
+++ rpc/proto/environment/environment.twirp.go
@@ -42,6 +42,10 @@ type Environment interface {
 	NewVariable(context.Context, *GeneralRequest) (*GeneralResponse, error)
 
 	AddVariable(context.Context, *GeneralRequest) (*GeneralResponse, error)
+
+	WriteToMemory(context.Context, *GeneralRequest) (*GeneralResponse, error)
+
+	ReadFromMemory(context.Context, *GeneralRequest) (*GeneralResponse, error)
 }
 
 // ===========================
@@ -50,19 +54,21 @@ type Environment interface {
 
 type environmentProtobufClient struct {
 	client HTTPClient
-	urls   [5]string
+	urls   [7]string
 }
 
 // NewEnvironmentProtobufClient creates a Protobuf client that implements the Environment interface.
 // It communicates using Protobuf and can be configured with a custom HTTPClient.
 func NewEnvironmentProtobufClient(addr string, client HTTPClient) Environment {
 	prefix := urlBase(addr) + EnvironmentPathPrefix
-	urls := [5]string{
+	urls := [7]string{
 		prefix + "NewEnvironment",
 		prefix + "QueryType",
 		prefix + "QueryValue",
 		prefix + "NewVariable",
 		prefix + "AddVariable",
+		prefix + "WriteToMemory",
+		prefix + "ReadFromMemory",
 	}
 	if httpClient, ok := client.(*http.Client); ok {
 		return &environmentProtobufClient{
@@ -136,25 +142,51 @@ func (c *environmentProtobufClient) AddVariable(ctx context.Context, in *General
 	return out, nil
 }
 
+func (c *environmentProtobufClient) WriteToMemory(ctx context.Context, in *GeneralRequest) (*GeneralResponse, error) {
+	ctx = ctxsetters.WithPackageName(ctx, "environment")
+	ctx = ctxsetters.WithServiceName(ctx, "Environment")
+	ctx = ctxsetters.WithMethodName(ctx, "WriteToMemory")
+	out := new(GeneralResponse)
+	err := doProtobufRequest(ctx, c.client, c.urls[5], in, out)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *environmentProtobufClient) ReadFromMemory(ctx context.Context, in *GeneralRequest) (*GeneralResponse, error) {
+	ctx = ctxsetters.WithPackageName(ctx, "environment")
+	ctx = ctxsetters.WithServiceName(ctx, "Environment")
+	ctx = ctxsetters.WithMethodName(ctx, "ReadFromMemory")
+	out := new(GeneralResponse)
+	err := doProtobufRequest(ctx, c.client, c.urls[6], in, out)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // =======================
 // Environment JSON Client
 // =======================
 
 type environmentJSONClient struct {
 	client HTTPClient
-	urls   [5]string
+	urls   [7]string
 }
 
 // NewEnvironmentJSONClient creates a JSON client that implements the Environment interface.
 // It communicates using JSON and can be configured with a custom HTTPClient.
 func NewEnvironmentJSONClient(addr string, client HTTPClient) Environment {
 	prefix := urlBase(addr) + EnvironmentPathPrefix
-	urls := [5]string{
+	urls := [7]string{
 		prefix + "NewEnvironment",
 		prefix + "QueryType",
 		prefix + "QueryValue",
 		prefix + "NewVariable",
 		prefix + "AddVariable",
+		prefix + "WriteToMemory",
+		prefix + "ReadFromMemory",
 	}
 	if httpClient, ok := client.(*http.Client); ok {
 		return &environmentJSONClient{
@@ -228,6 +260,30 @@ func (c *environmentJSONClient) AddVariable(ctx context.Context, in *GeneralRequ
 	return out, nil
 }
 
+func (c *environmentJSONClient) WriteToMemory(ctx context.Context, in *GeneralRequest) (*GeneralResponse, error) {
+	ctx = ctxsetters.WithPackageName(ctx, "environment")
+	ctx = ctxsetters.WithServiceName(ctx, "Environment")
+	ctx = ctxsetters.WithMethodName(ctx, "WriteToMemory")
+	out := new(GeneralResponse)
+	err := doJSONRequest(ctx, c.client, c.urls[5], in, out)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *environmentJSONClient) ReadFromMemory(ctx context.Context, in *GeneralRequest) (*GeneralResponse, error) {
+	ctx = ctxsetters.WithPackageName(ctx, "environment")
+	ctx = ctxsetters.WithServiceName(ctx, "Environment")
+	ctx = ctxsetters.WithMethodName(ctx, "ReadFromMemory")
+	out := new(GeneralResponse)
+	err := doJSONRequest(ctx, c.client, c.urls[6], in, out)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ==========================
 // Environment Server Handler
 // ==========================
@@ -290,6 +346,12 @@ func (s *environmentServer) ServeHTTP(resp http.ResponseWriter, req *http.Reques
 		return
 	case "/twirp/environment.Environment/AddVariable":
 		s.serveAddVariable(ctx, resp, req)
+		return
+	case "/twirp/environment.Environment/WriteToMemory":
+		s.serveWriteToMemory(ctx, resp, req)
+		return
+	case "/twirp/environment.Environment/ReadFromMemory":
+		s.serveReadFromMemory(ctx, resp, req)
 		return
 	default:
 		msg := fmt.Sprintf("no handler for path %q", req.URL.Path)
@@ -1019,6 +1081,294 @@ func (s *environmentServer) serveAddVariableProtobuf(ctx context.Context, resp h
 	callResponseSent(ctx, s.hooks)
 }
 
+func (s *environmentServer) serveWriteToMemory(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	header := req.Header.Get("Content-Type")
+	i := strings.Index(header, ";")
+	if i == -1 {
+		i = len(header)
+	}
+	switch strings.TrimSpace(strings.ToLower(header[:i])) {
+	case "application/json":
+		s.serveWriteToMemoryJSON(ctx, resp, req)
+	case "application/protobuf":
+		s.serveWriteToMemoryProtobuf(ctx, resp, req)
+	default:
+		msg := fmt.Sprintf("unexpected Content-Type: %q", req.Header.Get("Content-Type"))
+		twerr := badRouteError(msg, req.Method, req.URL.Path)
+		s.writeError(ctx, resp, twerr)
+	}
+}
+
+func (s *environmentServer) serveWriteToMemoryJSON(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	var err error
+	ctx = ctxsetters.WithMethodName(ctx, "WriteToMemory")
+	ctx, err = callRequestRouted(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	reqContent := new(GeneralRequest)
+	unmarshaler := jsonpb.Unmarshaler{AllowUnknownFields: true}
+	if err = unmarshaler.Unmarshal(req.Body, reqContent); err != nil {
+		err = wrapErr(err, "failed to parse request json")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	// Call service method
+	var respContent *GeneralResponse
+	func() {
+		defer func() {
+			// In case of a panic, serve a 500 error and then panic.
+			if r := recover(); r != nil {
+				s.writeError(ctx, resp, twirp.InternalError("Internal service panic"))
+				panic(r)
+			}
+		}()
+		respContent, err = s.Environment.WriteToMemory(ctx, reqContent)
+	}()
+
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+	if respContent == nil {
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *GeneralResponse and nil error while calling WriteToMemory. nil responses are not supported"))
+		return
+	}
+
+	ctx = callResponsePrepared(ctx, s.hooks)
+
+	var buf bytes.Buffer
+	marshaler := &jsonpb.Marshaler{OrigName: true}
+	if err = marshaler.Marshal(&buf, respContent); err != nil {
+		err = wrapErr(err, "failed to marshal json response")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	ctx = ctxsetters.WithStatusCode(ctx, http.StatusOK)
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(http.StatusOK)
+
+	respBytes := buf.Bytes()
+	if n, err := resp.Write(respBytes); err != nil {
+		msg := fmt.Sprintf("failed to write response, %d of %d bytes written: %s", n, len(respBytes), err.Error())
+		twerr := twirp.NewError(twirp.Unknown, msg)
+		callError(ctx, s.hooks, twerr)
+	}
+	callResponseSent(ctx, s.hooks)
+}
+
+func (s *environmentServer) serveWriteToMemoryProtobuf(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	var err error
+	ctx = ctxsetters.WithMethodName(ctx, "WriteToMemory")
+	ctx, err = callRequestRouted(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	buf, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		err = wrapErr(err, "failed to read request body")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+	reqContent := new(GeneralRequest)
+	if err = proto.Unmarshal(buf, reqContent); err != nil {
+		err = wrapErr(err, "failed to parse request proto")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	// Call service method
+	var respContent *GeneralResponse
+	func() {
+		defer func() {
+			// In case of a panic, serve a 500 error and then panic.
+			if r := recover(); r != nil {
+				s.writeError(ctx, resp, twirp.InternalError("Internal service panic"))
+				panic(r)
+			}
+		}()
+		respContent, err = s.Environment.WriteToMemory(ctx, reqContent)
+	}()
+
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+	if respContent == nil {
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *GeneralResponse and nil error while calling WriteToMemory. nil responses are not supported"))
+		return
+	}
+
+	ctx = callResponsePrepared(ctx, s.hooks)
+
+	respBytes, err := proto.Marshal(respContent)
+	if err != nil {
+		err = wrapErr(err, "failed to marshal proto response")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	ctx = ctxsetters.WithStatusCode(ctx, http.StatusOK)
+	resp.Header().Set("Content-Type", "application/protobuf")
+	resp.WriteHeader(http.StatusOK)
+	if n, err := resp.Write(respBytes); err != nil {
+		msg := fmt.Sprintf("failed to write response, %d of %d bytes written: %s", n, len(respBytes), err.Error())
+		twerr := twirp.NewError(twirp.Unknown, msg)
+		callError(ctx, s.hooks, twerr)
+	}
+	callResponseSent(ctx, s.hooks)
+}
+
+func (s *environmentServer) serveReadFromMemory(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	header := req.Header.Get("Content-Type")
+	i := strings.Index(header, ";")
+	if i == -1 {
+		i = len(header)
+	}
+	switch strings.TrimSpace(strings.ToLower(header[:i])) {
+	case "application/json":
+		s.serveReadFromMemoryJSON(ctx, resp, req)
+	case "application/protobuf":
+		s.serveReadFromMemoryProtobuf(ctx, resp, req)
+	default:
+		msg := fmt.Sprintf("unexpected Content-Type: %q", req.Header.Get("Content-Type"))
+		twerr := badRouteError(msg, req.Method, req.URL.Path)
+		s.writeError(ctx, resp, twerr)
+	}
+}
+
+func (s *environmentServer) serveReadFromMemoryJSON(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	var err error
+	ctx = ctxsetters.WithMethodName(ctx, "ReadFromMemory")
+	ctx, err = callRequestRouted(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	reqContent := new(GeneralRequest)
+	unmarshaler := jsonpb.Unmarshaler{AllowUnknownFields: true}
+	if err = unmarshaler.Unmarshal(req.Body, reqContent); err != nil {
+		err = wrapErr(err, "failed to parse request json")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	// Call service method
+	var respContent *GeneralResponse
+	func() {
+		defer func() {
+			// In case of a panic, serve a 500 error and then panic.
+			if r := recover(); r != nil {
+				s.writeError(ctx, resp, twirp.InternalError("Internal service panic"))
+				panic(r)
+			}
+		}()
+		respContent, err = s.Environment.ReadFromMemory(ctx, reqContent)
+	}()
+
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+	if respContent == nil {
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *GeneralResponse and nil error while calling ReadFromMemory. nil responses are not supported"))
+		return
+	}
+
+	ctx = callResponsePrepared(ctx, s.hooks)
+
+	var buf bytes.Buffer
+	marshaler := &jsonpb.Marshaler{OrigName: true}
+	if err = marshaler.Marshal(&buf, respContent); err != nil {
+		err = wrapErr(err, "failed to marshal json response")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	ctx = ctxsetters.WithStatusCode(ctx, http.StatusOK)
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(http.StatusOK)
+
+	respBytes := buf.Bytes()
+	if n, err := resp.Write(respBytes); err != nil {
+		msg := fmt.Sprintf("failed to write response, %d of %d bytes written: %s", n, len(respBytes), err.Error())
+		twerr := twirp.NewError(twirp.Unknown, msg)
+		callError(ctx, s.hooks, twerr)
+	}
+	callResponseSent(ctx, s.hooks)
+}
+
+func (s *environmentServer) serveReadFromMemoryProtobuf(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	var err error
+	ctx = ctxsetters.WithMethodName(ctx, "ReadFromMemory")
+	ctx, err = callRequestRouted(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	buf, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		err = wrapErr(err, "failed to read request body")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+	reqContent := new(GeneralRequest)
+	if err = proto.Unmarshal(buf, reqContent); err != nil {
+		err = wrapErr(err, "failed to parse request proto")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	// Call service method
+	var respContent *GeneralResponse
+	func() {
+		defer func() {
+			// In case of a panic, serve a 500 error and then panic.
+			if r := recover(); r != nil {
+				s.writeError(ctx, resp, twirp.InternalError("Internal service panic"))
+				panic(r)
+			}
+		}()
+		respContent, err = s.Environment.ReadFromMemory(ctx, reqContent)
+	}()
+
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+	if respContent == nil {
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *GeneralResponse and nil error while calling ReadFromMemory. nil responses are not supported"))
+		return
+	}
+
+	ctx = callResponsePrepared(ctx, s.hooks)
+
+	respBytes, err := proto.Marshal(respContent)
+	if err != nil {
+		err = wrapErr(err, "failed to marshal proto response")
+		s.writeError(ctx, resp, twirp.InternalErrorWith(err))
+		return
+	}
+
+	ctx = ctxsetters.WithStatusCode(ctx, http.StatusOK)
+	resp.Header().Set("Content-Type", "application/protobuf")
+	resp.WriteHeader(http.StatusOK)
+	if n, err := resp.Write(respBytes); err != nil {
+		msg := fmt.Sprintf("failed to write response, %d of %d bytes written: %s", n, len(respBytes), err.Error())
+		twerr := twirp.NewError(twirp.Unknown, msg)
+		callError(ctx, s.hooks, twerr)
+	}
+	callResponseSent(ctx, s.hooks)
+}
+
 func (s *environmentServer) ServiceDescriptor() ([]byte, int) {
 	return twirpFileDescriptor0, 0
 }
@@ -1448,22 +1798,24 @@ func callError(ctx context.Context, h *twirp.ServerHooks, err twirp.Error) conte
 }
 
 var twirpFileDescriptor0 = []byte{
-	// 266 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4c, 0xcd, 0x2b, 0xcb,
-	0x2c, 0xca, 0xcf, 0xcb, 0x4d, 0xcd, 0x2b, 0xd1, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x46,
-	0x12, 0x52, 0x5a, 0xc3, 0xc8, 0xc5, 0xe7, 0x9e, 0x9a, 0x97, 0x5a, 0x94, 0x98, 0x13, 0x94, 0x5a,
-	0x58, 0x9a, 0x5a, 0x5c, 0x22, 0xa4, 0xc4, 0xc5, 0x53, 0x96, 0x58, 0x94, 0x99, 0x98, 0x94, 0x93,
-	0x1a, 0x52, 0x59, 0x90, 0x2a, 0xc1, 0xa8, 0xc0, 0xa8, 0xc1, 0x19, 0x84, 0x22, 0x26, 0x24, 0xc2,
-	0xc5, 0x5a, 0x96, 0x98, 0x53, 0x9a, 0x2a, 0xc1, 0x04, 0x96, 0x84, 0x70, 0x90, 0x75, 0xfa, 0x25,
-	0xe6, 0xa6, 0x4a, 0x30, 0xa3, 0xea, 0x04, 0x89, 0x09, 0x69, 0x70, 0xf1, 0x17, 0xa5, 0x16, 0xe4,
-	0x24, 0x26, 0xa7, 0xba, 0x56, 0x64, 0x16, 0x97, 0x64, 0xe6, 0xa5, 0x4b, 0xb0, 0x28, 0x30, 0x6a,
-	0x70, 0x04, 0xa1, 0x0b, 0x0b, 0x09, 0x71, 0xb1, 0x14, 0x24, 0x96, 0x64, 0x48, 0xb0, 0x82, 0x4d,
-	0x01, 0xb3, 0x95, 0xb4, 0xb9, 0xf8, 0xe1, 0xae, 0x2d, 0x2e, 0xc8, 0xcf, 0x2b, 0x4e, 0x15, 0x92,
-	0xe0, 0x62, 0xcf, 0x4d, 0x2d, 0x2e, 0x4e, 0x4c, 0x87, 0xb9, 0x14, 0xc6, 0x35, 0xea, 0x64, 0xe6,
-	0xe2, 0x76, 0x45, 0xf8, 0x55, 0xc8, 0x97, 0x8b, 0xcf, 0x2f, 0xb5, 0x1c, 0x59, 0x44, 0x5a, 0x0f,
-	0x39, 0x78, 0x50, 0xc3, 0x41, 0x4a, 0x06, 0xbb, 0x24, 0xc4, 0x5a, 0x25, 0x06, 0x21, 0x0f, 0x2e,
-	0xce, 0xc0, 0xd2, 0xd4, 0xa2, 0x4a, 0x70, 0x80, 0x50, 0x64, 0x92, 0x27, 0x17, 0x17, 0xd8, 0xa4,
-	0x30, 0x70, 0x28, 0x52, 0x64, 0x94, 0x17, 0x17, 0xb7, 0x5f, 0x6a, 0x79, 0x18, 0x34, 0xc4, 0x29,
-	0x36, 0xcb, 0x31, 0x25, 0x85, 0x2a, 0x66, 0x25, 0xb1, 0x81, 0xd3, 0x9e, 0x31, 0x20, 0x00, 0x00,
-	0xff, 0xff, 0x03, 0xd9, 0x49, 0x92, 0x90, 0x02, 0x00, 0x00,
+	// 290 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x93, 0xcd, 0x4a, 0xc3, 0x40,
+	0x10, 0xc7, 0x8d, 0xb6, 0x6a, 0x27, 0x9a, 0xe2, 0xe2, 0x61, 0x51, 0x0f, 0x25, 0xa7, 0x80, 0xd0,
+	0x83, 0x3e, 0x81, 0x87, 0xfa, 0x85, 0x09, 0x18, 0x4a, 0x3d, 0x6f, 0xcd, 0x50, 0x03, 0xd9, 0x0f,
+	0x77, 0x37, 0xa9, 0x79, 0x21, 0x4f, 0x3e, 0xa4, 0xb8, 0xb5, 0x9a, 0x88, 0xb7, 0xf4, 0xb6, 0xf3,
+	0x1f, 0xe6, 0xb7, 0xb3, 0x3f, 0x58, 0x38, 0x42, 0x51, 0xe5, 0x5a, 0x0a, 0x8e, 0xc2, 0x8e, 0x95,
+	0x96, 0x56, 0x12, 0xbf, 0x11, 0x85, 0x1f, 0x1e, 0x04, 0x37, 0x28, 0x50, 0xb3, 0x22, 0xc5, 0xd7,
+	0x12, 0x8d, 0x25, 0x21, 0x1c, 0x54, 0x4c, 0xe7, 0x6c, 0x5e, 0xe0, 0xb4, 0x56, 0x48, 0xbd, 0x91,
+	0x17, 0x0d, 0xd2, 0x56, 0x46, 0x8e, 0xa1, 0x5f, 0xb1, 0xa2, 0x44, 0xba, 0xed, 0x9a, 0xab, 0xa2,
+	0x39, 0x99, 0x30, 0x8e, 0x74, 0xa7, 0x3d, 0xf9, 0x95, 0x91, 0x08, 0x86, 0x1a, 0x55, 0xc1, 0x9e,
+	0x71, 0xf2, 0x96, 0x1b, 0x9b, 0x8b, 0x05, 0xed, 0x8d, 0xbc, 0x68, 0x3f, 0xfd, 0x1b, 0x13, 0x02,
+	0x3d, 0xc5, 0xec, 0x0b, 0xed, 0x3b, 0x8a, 0x3b, 0x87, 0xe7, 0x30, 0xfc, 0xd9, 0xd6, 0x28, 0x29,
+	0x0c, 0x12, 0x0a, 0x7b, 0x1c, 0x8d, 0x61, 0x8b, 0xf5, 0xa6, 0xeb, 0xf2, 0xe2, 0xbd, 0x07, 0xfe,
+	0xe4, 0xf7, 0xad, 0x24, 0x86, 0x20, 0xc1, 0x65, 0x33, 0x39, 0x1d, 0x37, 0xf5, 0xb4, 0x3d, 0x9c,
+	0x9c, 0xfd, 0xdf, 0x5c, 0x5d, 0x1b, 0x6e, 0x91, 0x5b, 0x18, 0x3c, 0x96, 0xa8, 0x6b, 0x27, 0xa4,
+	0x13, 0xe9, 0x0e, 0xc0, 0x91, 0x66, 0xce, 0x62, 0x27, 0xd4, 0x3d, 0xf8, 0x09, 0x2e, 0x67, 0xdf,
+	0xc6, 0x3b, 0xb3, 0xae, 0xb2, 0x6c, 0x33, 0xac, 0x07, 0x38, 0x7c, 0xd2, 0xb9, 0xc5, 0xa9, 0x8c,
+	0x91, 0x4b, 0x5d, 0x77, 0xa3, 0xc5, 0x10, 0xa4, 0xc8, 0xb2, 0x6b, 0x2d, 0xf9, 0x06, 0x70, 0xf3,
+	0x5d, 0xf7, 0x31, 0x2e, 0x3f, 0x03, 0x00, 0x00, 0xff, 0xff, 0x53, 0x86, 0x9a, 0xc1, 0x2d, 0x03,
+	0x00, 0x00,
 }
