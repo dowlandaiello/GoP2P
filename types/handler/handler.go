ddb80d53f4188652c01496a862bc6e58e236d07a
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-25 13:23:24 -0400

    Dep ensured.

--- types/handler/handler.go
+++ types/handler/handler.go
@@ -60,7 +60,7 @@ func handleConnection(node *node.Node, conn net.Conn) error {
 			return err // Return found error
 		}
 
-		fmt.Println("\n-- CONNECTION " + conn.RemoteAddr().String() + " -- responding with data " + common.SafeSlice(serializedResponse) + "...")
+		fmt.Println("\n-- CONNECTION " + conn.RemoteAddr().String() + " -- responding with data " + common.SafeSlice(serializedResponse) + "...") // Log response
 
 		conn.Write(serializedResponse) // Write success
 
@@ -81,7 +81,7 @@ func handleConnection(node *node.Node, conn net.Conn) error {
 		return err // Return found error
 	}
 
-	fmt.Println("\n-- CONNECTION " + readConnection.InitializationNode.Address + " -- responding with data " + common.SafeSlice(serializedResponse) + "...")
+	fmt.Println("\n-- CONNECTION " + readConnection.InitializationNode.Address + " -- responding with data " + common.SafeSlice(serializedResponse) + "...") // Log response
 
 	conn.Write(serializedResponse) // Write success
 
