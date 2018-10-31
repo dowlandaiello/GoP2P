14d2f367ada8abffcc6345c4ec212a27736599b9
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-31 15:39:15 -0400

    :shrug:

--- main_test.go
+++ main_test.go
@@ -13,23 +13,23 @@ func TestMain(t *testing.T) {
 	currentDir, err := common.GetCurrentDir() // Get working directory
 
 	if err != nil { // Check for errors
-		t.Errorf(err.Error(), err.Error()) // Log found error
-		t.FailNow()                        // Panic
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
 	}
 
 	node, err := node.NewNode("1.1.1.1", false) // Init node
 
 	if err != nil && !strings.Contains(err.Error(), "not permitted") { // Check for errors
-		t.Errorf(err.Error(), err.Error()) // Log found error
-		t.FailNow()                        // Panic
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
 	} else if err != nil && strings.Contains(err.Error(), "not permitted") { // Check for perm errors
 		t.Logf("WARNING: node testing requires sudo privileges") // Log sudo required
 	} else {
 		err = node.WriteToMemory(currentDir) // Write to working directory
 
 		if err != nil { // Check for errors
-			t.Errorf(err.Error(), err.Error()) // Log found error
-			t.FailNow()                        // Panic
+			t.Errorf(err.Error()) // Log found error
+			t.FailNow()           // Panic
 		}
 
 		main() // :shrug:
