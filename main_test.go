6e075a92c1b42cb45e68110bccb070c145d1a8c2
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-31 13:10:15 -0400

    Added main coverage.

--- main_test.go
+++ main_test.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/mitsukomegumi/GoP2P/common"
+	"github.com/mitsukomegumi/GoP2P/types/node"
+)
+
+// TestMain - test functionality of main() method
+func TestMain(t *testing.T) {
+	currentDir, err := common.GetCurrentDir() // Get working directory
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error(), err.Error()) // Log found error
+		t.FailNow()                        // Panic
+	}
+
+	node, err := node.NewNode("1.1.1.1", false) // Init node
+
+	if err != nil && !strings.Contains(err.Error(), "not permitted") { // Check for errors
+		t.Errorf(err.Error(), err.Error()) // Log found error
+		t.FailNow()                        // Panic
+	} else if err != nil && strings.Contains(err.Error(), "not permitted") { // Check for perm errors
+		t.Logf("WARNING: node testing requires sudo privileges") // Log sudo required
+	} else {
+		err = node.WriteToMemory(currentDir) // Write to working directory
+
+		if err != nil { // Check for errors
+			t.Errorf(err.Error(), err.Error()) // Log found error
+			t.FailNow()                        // Panic
+		}
+
+		main() // :shrug:
+	}
+}
+
+// TestStartRPCServer - test functionality of startRPCServer() method
+func TestStartRPCServer(t *testing.T) {
+	startRPCServer() // Start RPC server
+}
+
+// TestStartNode - test functionality of startNode() method
+func TestStartNode(t *testing.T) {
+	startNode() // Start local node
+}
