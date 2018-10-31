0934347185e333a4bf99bb42be4318918f31c64c
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-10-31 08:59:09 -0400

    Added forwardPortSilent coverage.

--- upnp/upnp_test.go
+++ upnp/upnp_test.go
@@ -7,7 +7,21 @@ import (
 
 // TestDiscoverGateway - test functionality of gateway discovery
 func TestDiscoverGateway(t *testing.T) {
-	_, err := GetGateway() // Attempt to fetch gateway device
+	gateway, err := GetGateway() // Attempt to fetch gateway device
+
+	if err != nil && !strings.Contains(err.Error(), "no UPnP") {
+		t.Errorf(err.Error()) // If error occurs, print error to testing console
+		t.FailNow()
+	} else if err != nil && strings.Contains(err.Error(), "no UPnP") {
+		t.Logf("WARNING: UPnP testing requires UPnP network support") // Log warning
+	} else {
+		t.Logf("found gateway location: " + gateway.Location()) // Log gateway
+	}
+}
+
+// TestForwardPort - test functionality of port forwarding
+func TestForwardPort(t *testing.T) {
+	err := ForwardPort(3000) // Attempt to forward port 3000
 
 	if err != nil && !strings.Contains(err.Error(), "no UPnP") {
 		t.Errorf(err.Error()) // If error occurs, print error to testing console
@@ -17,9 +31,9 @@ func TestDiscoverGateway(t *testing.T) {
 	}
 }
 
-// TestForwardPort - test functionality of port forwarding
-func TestForwardPort(t *testing.T) {
-	err := ForwardPort(3000) // Attempt to forward port 3000
+// TestForwardPortSilent - test functionality of silent port forwarding
+func TestForwardPortSilent(t *testing.T) {
+	err := ForwardPortSilent(3000) // Attempt to forward port 3000
 
 	if err != nil && !strings.Contains(err.Error(), "no UPnP") {
 		t.Errorf(err.Error()) // If error occurs, print error to testing console
