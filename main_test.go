5b9baf79f8684b7e6c57bbec8992dd147f82c4df
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-11-01 08:02:37 -0400

    Finalized StartNode, StartRPCServer coverage.

--- main_test.go
+++ main_test.go
@@ -43,5 +43,28 @@ func TestStartRPCServer(t *testing.T) {
 
 // TestStartNode - test functionality of startNode() method
 func TestStartNode(t *testing.T) {
-	startNode() // Start local node
+	currentDir, err := common.GetCurrentDir() // Get working directory
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	node, err := node.NewNode("1.1.1.1", false) // Init node
+
+	if err != nil && !strings.Contains(err.Error(), "not permitted") { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	} else if err != nil && strings.Contains(err.Error(), "not permitted") { // Check for perm errors
+		t.Logf("WARNING: node testing requires sudo privileges") // Log sudo required
+	} else {
+		err = node.WriteToMemory(currentDir) // Write to working directory
+
+		if err != nil { // Check for errors
+			t.Errorf(err.Error()) // Log found error
+			t.FailNow()           // Panic
+		}
+
+		go startNode() // Start local node
+	}
 }
