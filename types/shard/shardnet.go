f416422c430fc90efad89e7d6e62d3e3fcaddf3b
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-11-01 20:36:04 -0400

    Debugging? :shrug:

--- types/shard/shardnet.go
+++ types/shard/shardnet.go
@@ -25,17 +25,20 @@ func SendBytesShardResult(b []byte, address string, port int) ([][]byte, error) 
 	startTime := time.Now() // Get current time
 
 	for _, address := range addresses { // Iterate through addresses
-		address = address + ":" + strconv.Itoa(port) // Append port
-
 		if float64(len(finished)) > (0.51 * float64(len(addresses))) { // Check 51% of nodes finished
 			return buffer, nil // Return read data
 		}
 
+		address = address + ":" + strconv.Itoa(port) // Append port
+
 		go common.SendBytesResultBufferAsync(b, buffer, address, finished) // Send to address, append to buffer
 	}
 
-	for float64(len(finished)) > (0.51 * float64(len(addresses))) {
-		if time.Now().Sub(startTime) > 3 { // Check for time out
+	for float64(len(finished)) < (0.51 * float64(len(addresses))) {
+		fmt.Println(len(finished))
+		fmt.Println(0.51 * float64(len(addresses)))
+
+		if time.Now().Sub(startTime) > 3*time.Second { // Check for time out
 			return [][]byte{}, errors.New("timed out") // Return timed out
 		}
 	}
