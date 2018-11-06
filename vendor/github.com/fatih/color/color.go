876a3f7942d0e216612d04e37445bb27619f2130
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-11-06 11:03:04 -0500

    Built wasm.

--- vendor/github.com/fatih/color/color.go
+++ vendor/github.com/fatih/color/color.go
@@ -3,13 +3,11 @@ package color
 import (
 	"fmt"
 	"io"
-	"os"
 	"strconv"
 	"strings"
 	"sync"
 
 	"github.com/mattn/go-colorable"
-	"github.com/mattn/go-isatty"
 )
 
 var (
@@ -17,8 +15,7 @@ var (
 	// false or true based on the stdout's file descriptor referring to a terminal
 	// or not. This is a global option and affects all colors. For more control
 	// over each color block use the methods DisableColor() individually.
-	NoColor = os.Getenv("TERM") == "dumb" ||
-		(!isatty.IsTerminal(os.Stdout.Fd()) && !isatty.IsCygwinTerminal(os.Stdout.Fd()))
+	NoColor = false
 
 	// Output defines the standard output of the print functions. By default
 	// os.Stdout is used.
