b0801f4ff85136828d7ce0bee4ddad0f62e9c47b
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-11-23 16:59:27 -0500

    Init protobufHandler tests.

--- types/handler/handler_test.go
+++ types/handler/handler_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -9,6 +10,7 @@ import (
 	"github.com/mitsukomegumi/GoP2P/types/node"
 )
 
+// TestStartHandler - test functionality of StartHandler
 func TestStartHandler(t *testing.T) {
 	node, err := newNodeSafe() // Attempt to create new node
 
@@ -36,6 +38,41 @@ func TestStartHandler(t *testing.T) {
 	}()
 
 	t.Logf("started handler with listener address %s", (*ln).Addr()) // Log success
+}
+
+// TestStartProtobufHandler - test functionality of StartProtobufHandler
+func TestStartProtobufHandler(t *testing.T) {
+	node, err := newNodeSafe() // Attempt to create new node
+
+	if err != nil && !strings.Contains(err.Error(), "root") { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	} else if err != nil { // Account for special case
+		t.Logf(err.Error()) // Log found error
+	}
+
+	ln, err := node.StartListener(3000) // Start listener
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	go func() {
+		err = StartProtobufHandler(testHandler, "test", ln) // Attempt to start handler
+
+		if err != nil { // Check for error
+			t.Errorf(err.Error()) // Log found error
+			t.FailNow()           // Panic
+		}
+	}()
+}
+
+// testHandler - test handler
+func testHandler(b []byte) error {
+	fmt.Println(string(b)) // Log input
+
+	return nil // No error occurred
 }
 
 func newNodeSafe() (*node.Node, error) {
